8774225d24429c5051a3dfdd6a622df3f4e20ab4
sofiaml <sofiamendezlaferrara@gmail.com>
2025-09-08 00:20:26 -0300

    correccion act 1 y test, anda todo amigas :))

--- TrabajoPractico_1/proyecto_1/modules/ListaDobleEnlazada.py
+++ TrabajoPractico_1/proyecto_1/modules/ListaDobleEnlazada.py
@@ -64,38 +64,54 @@ class ListaDobleEnlazada:
             raise IndexError("La posicion esta fuera del rango")
 
     def extraer(self, posicion=None):
-        if self.esta_vacia():
-            raise IndexError("La lista esta vacia, no se puede extraer")
-        
-        if posicion is None or posicion == self.tamanio -1:  #eliminar el ultimo nodo
-            dato = self.cola.dato 
-            if self.tamanio == 1:       #si solo tiene un nodo
-                self.cabeza = self.cola = None
-            else:                        #si tiene mas de un nodo
-                self.cola = self.cola.anterior 
-                self.cola.siguiente = None
-            self.tamanio -= 1
-            return dato
-        
-        elif posicion == 0:     #caso eliminar el primer nodo 
-            dato = self.cabeza.dato
-            self.cabeza = self.cabeza.siguiente
-            if self.cabeza:
-                self.cabeza.anterior = None
+            if self.esta_vacia():
+                raise IndexError("La lista está vacía, no se puede extraer")
+
+            # si no se pasa posición → eliminar último
+            if posicion is None:
+                posicion = self.tamanio - 1
+
+            # permitir índices negativos
+            if posicion < 0:
+                posicion = self.tamanio + posicion
+
+            # chequeo de rango después de ajustar el índice
+            if posicion < 0 or posicion >= self.tamanio:
+                raise IndexError("La posición está fuera de rango")
+
+            # caso eliminar cabeza
+            if posicion == 0:
+                dato = self.cabeza.dato
+                self.cabeza = self.cabeza.siguiente
+                if self.cabeza:
+                    self.cabeza.anterior = None
+                else:
+                    self.cola = None  # lista quedó vacía
+
+            # caso eliminar cola
+            elif posicion == self.tamanio - 1:
+                dato = self.cola.dato
+                self.cola = self.cola.anterior
+                if self.cola:
+                    self.cola.siguiente = None
+                else:
+                    self.cabeza = None  # lista quedó vacía
+
+            # caso eliminar nodo intermedio
             else:
-                self.cola = None
+                nodo_actual = self.cabeza
+                for _ in range(posicion):
+                    nodo_actual = nodo_actual.siguiente
+                dato = nodo_actual.dato
+                nodo_actual.anterior.siguiente = nodo_actual.siguiente
+                nodo_actual.siguiente.anterior = nodo_actual.anterior
+
             self.tamanio -= 1
             return dato
-        
-        elif 0 < posicion < self.tamanio -1:   #caso eliminar nodo en posicion intermedia
-            nodo_actual = self.cabeza
-            for _ in range(posicion):
-                nodo_actual = nodo_actual.siguiente
-            dato = nodo_actual.dato
-            nodo_actual.anterior.siguiente = nodo_actual.siguiente
-            nodo_actual.siguiente.anterior = nodo_actual.anterior
-            self.tamanio -= 1
-            return dato
+    
+
+    
+  
         
     def copiar(self):
         copia_lista= ListaDobleEnlazada()
@@ -104,25 +120,28 @@ class ListaDobleEnlazada:
         return copia_lista
     
     def invertir(self):
-        lista_invertida= ListaDobleEnlazada()
-        for dato in self:
-            lista_invertida.agregar_al_inicio(dato)
-        return lista_invertida
+        actual = self.cabeza
+        self.cabeza, self.cola = self.cola, self.cabeza  # intercambiamos extremos
+        while actual:
+            actual.siguiente, actual.anterior = actual.anterior, actual.siguiente
+            actual = actual.anterior
+    #invierte la misma lista, no crea otra, por lo que dice el enunciado deberia ser asi.
     
-    def concatenar(self, nueva_lista):
-        otra_lista= nueva_lista.copia()
-        for dato in nueva_lista:
-            otra_lista.agregar_al_final(dato)
-        return otra_lista
+    def concatenar(self, otra_lista): #devuelve lista actual con la otra concatenada al final
+        for dato in otra_lista:       #la self se modifica y desp devuelve, no deja originales intactas
+            self.agregar_al_final(dato)
+        return self
+
     
-    def len(self):
+    def __len__(self):
         return self.tamanio
     
     def __add__(self, otra_lista):
-        nueva_lista= self.copiar()
-        for dato in otra_lista:
-            nueva_lista.agregar_al_final(dato)
+        nueva_lista = self.copiar()       # copiamos self
+        nueva_lista.concatenar(otra_lista)  # concatenamos la otra
         return nueva_lista
+    #lo cambie para q no modifique las listas originales y devuelva otra
+
     
     def __iter__(self):
         nodo_actual = self.cabeza
@@ -130,7 +149,16 @@ class ListaDobleEnlazada:
             yield nodo_actual.dato
             nodo_actual = nodo_actual.siguiente
 
-
+    def agregar_al_final(self, dato):
+        nuevo_nodo = Nodo(dato)
+        if self.esta_vacia():
+            self.cabeza = self.cola = nuevo_nodo
+        else:
+            nuevo_nodo.anterior = self.cola
+            self.cola.siguiente = nuevo_nodo
+            self.cola = nuevo_nodo
+        self.tamanio += 1 
+    # si la lista esta vacia, asigna cabeza y cola al mismo nodo, sino conecta y actualiza la cola
         
 
 
